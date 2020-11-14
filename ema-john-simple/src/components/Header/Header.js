dd21267985f7c33c29b6cb6a5ef0fa263f9717cd
SumonWebDev <sumon.mathjnu@gmail.com>
2020-11-14 14:37:45 +0600

    sending file

---
+++ ema-john-simple/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.png';
+import header from './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>This Is Header</h1>
+            <img src={logo} alt=""/>
+            <nav>
+                <a href="Shop">Shop</a>
+                <a href="Review">Review</a>
+                <a href="Manage">Manage-Inventery</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
